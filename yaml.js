a09b4b202a6c474fec9581c0806f04c128100aa1
blackchoey <blackchoey@gmail.com>
2022-12-22 14:25:50 +0800

    test

--- yaml.js
+++ yaml.js
@@ -1,1 +1,11 @@
 const YAML = require('yaml');
+const fs = require('fs');
+
+const str = fs.readFileSync('app.yml', 'utf-8');
+const doc = YAML.parseDocument(str);
+// console.log(JSON.stringify(doc));
+
+// doc.add(doc.createNode({projectId:"abcd"}));
+// doc.add(doc.createPair("projectId", "abcde"));
+console.log(doc.toJS().projectId);
+fs.writeFileSync('app2.yml', doc.toString());
